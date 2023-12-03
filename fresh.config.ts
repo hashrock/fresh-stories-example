6dd24ae5b40755d54b4190ec6d4730edcc78bd43
hashrock <hashrock@users.noreply.github.com>
2023-12-03 23:17:49 +0900

    bump version

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
-import storiesPlugin from "https://deno.land/x/fresh_stories@0.0.3/stories-plugin.ts";
+import storiesPlugin from "https://deno.land/x/fresh_stories@0.0.4/stories-plugin.ts";
 
 export default defineConfig({
   plugins: [tailwind(), storiesPlugin()],
