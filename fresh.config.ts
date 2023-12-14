12093674689f96493765c53c25316fb18336e8d0
hashrock <hashrock@users.noreply.github.com>
2023-12-15 03:16:15 +0900

    revert

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
-import storiesPlugin from "https://deno.land/x/fresh_stories@0.0.5/stories-plugin.ts";
+import storiesPlugin from "https://deno.land/x/fresh_stories@0.0.10/stories-plugin.ts";
 
 export default defineConfig({
   plugins: [storiesPlugin(), tailwind()],
